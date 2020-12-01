963ade1c428c027614156a0ac1acb44afa6bafbd
Sabz <sabrina.d.mello@hotmail.com>
2020-12-01 09:23:14 -0500

    Added Challenge 01

---
+++ AdventOfCode/Source/Challenges/Challenge_01.cpp
@@ -0,0 +1,57 @@
+#include "Challenge_01.h"
+#include "../Helpers/FileHelpers.h"
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+std::string const CChallenge_01::sm_inputFilePath = "Inputs/Input_Challenge_01.txt";
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+EErrorCode CChallenge_01::SetUp_FirstPart()
+{
+    std::vector<std::string> lines;
+    EErrorCode const readErrorCode = FileHelper::ReadLines(sm_inputFilePath, lines);
+    if (readErrorCode != EErrorCode::Success)
+    {
+        return readErrorCode;
+    }
+
+    return EErrorCode::NotImplemented;
+}
+
+EErrorCode CChallenge_01::Run_FirstPart()
+{
+    return EErrorCode::NotImplemented;
+}
+
+EErrorCode CChallenge_01::CleanUp_FirstPart()
+{
+    return EErrorCode::NotImplemented;
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+EErrorCode CChallenge_01::SetUp_SecondPart()
+{
+    std::vector<std::string> lines;
+    EErrorCode const readErrorCode = FileHelper::ReadLines(sm_inputFilePath, lines);
+    if (readErrorCode != EErrorCode::Success)
+    {
+        return readErrorCode;
+    }
+    
+    return EErrorCode::NotImplemented;
+}
+
+EErrorCode CChallenge_01::Run_SecondPart()
+{
+    return EErrorCode::NotImplemented;
+}
+
+EErrorCode CChallenge_01::CleanUp_SecondPart()
+{
+    return EErrorCode::NotImplemented;
+}
